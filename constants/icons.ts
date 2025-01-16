4eeff36bcfb4c6bab37781fbe1e898b62c211447
Nbruchi <nkundabagenzibruce@gmail.com>
2025-01-16 08:59:37 +0200

    Changed home image

---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import calendar from "@/assets/icons/calendar.png";
+import document from "@/assets/icons/document.png";
+import search from "@/assets/icons/search.png";
+import today from "@/assets/icons/today.png";
+
+export default {
+    calendar,
+    document,
+    search,
+    today,
+};
